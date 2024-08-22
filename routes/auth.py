2826ab70a8df8b1d0d5f6234daa8ec9bb16d787e
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-08-22 03:38:41 +0300

    - Added a middleware dependency to retrieve user data from the PostgreSQL database based on the user's token. - Added a new route to retrieve user data using the UID extracted from the user's token.

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,8 @@
 import uuid
 import bcrypt
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Header
 from database import get_db
+from middleware.auth_middleware import auth_middleware
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
@@ -43,3 +44,12 @@ def login_user(user: UserLogin, db: Session = Depends(get_db)):
     token = jwt.encode({'id': user_db.id}, 'password_key')
 
     return {'token': token, 'user': user_db}
+
+
+@router.get('/')
+def current_user_data(db: Session = Depends(get_db), user_dict=Depends(auth_middleware)):
+
+    user = db.query(User).filter(User.id == user_dict['uid']).first()
+    if not user:
+        raise HTTPException(404, 'User not found!')
+    return user
